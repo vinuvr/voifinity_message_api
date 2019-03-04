27b0e9aede7646c6efa3e034124e49503a1f28b1
“vinuvr” <vr.vinu849@gmail.com>
2019-03-04 19:03:51 +0530

    updated code

--- src/voifinity_message_api_app.erl
+++ src/voifinity_message_api_app.erl
@@ -13,31 +13,20 @@
 start(_StartType, _StartArgs) ->
   Dispatch = cowboy_router:compile([         
             {'_', [
-                {"/user/delete_message/[...]",voifinity_message_deletemsg_handler, []},
-                {"/message_log/account/[...]",voifinity_message_msglog_account_handler, []},
-                {"/message_log/group/[...]",vofinity_message_msglog_group_handler, []},
-                {"/message_log/user/[...]", voifinity_message_msglog_user_handler, []},
-                {"/group_details/[...]",voifinity_message_group_details_handler, []},
-                {"/media/[...]",voifinity_message_fileupload_handler, []},
+                {"/delete_message/[...]",voifinity_message_deletemsg_handler, []},
+                {"/account/[...]",voifinity_message_account_handler, []},
+                {"/group/[...]",voifinity_message_group_handler, []},
+                {"/user/[...]", voifinity_message_user_handler, []},
+                {"/media/[...]",voifinity_message_fileupload_handler, []}
                   ]}     
             ]), 
-%  Dispatch = cowboy_router:compile([
-%            {'_', [{"/[...]", emqx_api_handler, []}]}
-%             ]),
   {ok, _} = cowboy:start_clear(my_http_listener,
         [{port, 9080}],
-        #{env => #{dispatch => Dispatch}}
-  ),
-   voifinity_message_api_sup:start_link().
-
+        #{env => #{dispatch => Dispatch}}),
+  voifinity_message_api_sup:start_link().
 %%--------------------------------------------------------------------
-
-
 stop(_State) ->
   ok.
-
-
-
 %%====================================================================
 %%%% Internal functions
 %%%%%====================================================================
